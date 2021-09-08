f7f358b2e9a4ea3b4eea8165e76f95f17dffbcc5
Kevin Sullivan <sullivan@virginia.edu>
2021-09-08 18:19:04 +0000

    Add/update lecture 4.

--- src/instructor/lectures/lecture_4.lean
+++ src/instructor/lectures/lecture_4.lean
@@ -1,16 +1,32 @@
 /-
-In this file, we give formal statements (our version)
-of the two axioms of equality. We also present Lean's
-versions of these rules, and show how you can use them
-without giving all of the arguments explicitly.
+We've seen that logics start with axioms that
+can then be combined (with other information)
+using *inference rules* to derive theorems. In
+this file we review what we've covered so far
+and then we introduce:
+
+(1) The concept of introduction and elimination
+rules for a given logical construct.
+(2) We distinguish the reflexivity axiom as an
+*introduction* rule (one that produces a proof
+of an equality), and the substitutability of
+equals as an *elimination* rule (one that uses,
+or consumes) a proof of an equality to produce
+some other kind of result.
+(3) We explicitly identity the introduction 
+rules for ∀ and for →. To produce a proof of
+∀ (x : T), P x (where T is a type and P is a
+predicate that asserts some property of x), we
+*assume* that we're given an arbitrary but
+specific (x : T) ["x of type T"], and then 
+we prove (P x) *for that x*. Because we made
+no assumptions whatsoever about x, if we can
+show that (P x) is true, then it must be true
+*for all* (x : T).
 -/
 
 /-
-AXIOMS
-
-Everything is equal to itself. A bit more formally,
-if one is given a type, T, and a value, t, of this
-type, then you may have a proof of t = t "for free."
+Introduction rule for equality. 
 -/
 
 axiom eq_refl  : 
@@ -19,12 +35,7 @@ axiom eq_refl  :
   t = t         -- the result type: proof of t = t
 
 /-
-INFERENCE RULE #2/2: SUBSTITUTION OF EQUALS FOR EQUALS
-
-Given any type, T, of objects, and any *property*, P,
-of objects of this type, if you know x has property P
-(written as P x) and you know that x = y, then you can
-deduce that y has property P.
+Elimination rule for equality.
 -/
 axiom eq_subst : 
   ∀ (T : Type)      -- if T is a type
@@ -42,24 +53,17 @@ details come later.
 -/
 
 /-
-We will now present formal proofs of our two 
-theorems in this style.
+CONJECTURES (review)
 -/
 
-/-
-CONJECTURES
--/
-
--- We define eq_symm to be the proposition at issue
--- Note: Prop is the type of all propositions in Lean
--- And each proposition is itself a type: of it proofs
+-- A conjecture: equality is symmetric.
 def eq_symm : Prop := 
   ∀ (T : Type) 
     (x y : T), 
     x = y → 
     y = x 
 
--- We define eq_trans formally in the same basic way
+-- A conjecture: equality is transitive.
 def eq_trans : Prop := 
   ∀ (T : Type) 
     (x y z : T), 
@@ -72,56 +76,41 @@ def eq_trans : Prop :=
 PROOFS: From conjectures to theorems
 -/
 
-/-
-Proofs: equality is symmetric.
--/
-
-/-
-def eq_symm : Prop := 
-  ∀ (T : Type) 
-    (x y : T), 
-    x = y → 
-    y = x 
-    -/
 
 example : eq_symm :=
 begin
   unfold eq_symm, -- replace name with definition
-  assume T x y e, -- assume arbitrary values
-  rw e,           -- rw uses eq.subst to replace x with y
-                  -- and then applies eq.refl automatically
+  assume T x y e, -- Introduction rule for ∀
+  rw e,           -- Elimination rule for =
   -- QED.
 end
 
 /-
-A fundamental idea is that any proof is as good as any
-other for establishing the truth of a proposition. Here
-is an equally good alternative proof (or to be correct,
-proof-generating scripts in the "proof tactic" language" 
-of the Lean Prover.
+A different proof, now using eq.subst explicitly.
+Any proof of a proposition is as good as any other
+for showing the truth of a proposition. We do not
+care what proofs you give, as long as they're correct
+(unless stated otherwise).
 -/
 
 example : eq_symm :=
 begin
   unfold eq_symm,   -- replace name with definition
-  assume T x y e,   -- assume arbitrary values
-  apply eq.subst e, -- apply axiom 2, substitutability
-  exact eq.refl x,  -- apply axiom 1, reflexivity
+  assume T x y e,   -- introduction rule for ∀ 
+  apply eq.subst e, -- elimination rule for =
+  exact eq.refl x,  -- introduction rule for =
   -- QED.
 end
 
 /-
-Proof: equality is transitive.
+Review: Proof: equality is transitive.
 -/
 
 example : eq_trans := 
 begin
   unfold eq_trans,
-  assume T,
-  assume x y z,
-  assume e1,
-  assume e2,
-  rw e1,
+  assume T x y z e1 e2, -- introduction rule for ∀ 
+  rw e1,                -- elimination rule for =
   exact e2,
 end
 
@@ -137,23 +126,26 @@ Note: Lean defines these rules as
 Practice
 -/
 
-example : ∀ (T : Type), ∀ (x y z : T), x = y → y = z → z = x :=
+example : ∀ (T : Type) (x y z : T), x = y → y = z → z = x :=
 begin
-
-  /-
-  assume T,
-  assume (x : T),
-  assume (y : T),
-  assume (z : T),
-  -/
-  assume T x y z,
-  assume h1 h2,
-  apply eq.symm _,
-  apply eq.trans h1 h2,
-
+  assume T x y z h1 h2, -- introduction rule for ∀ 
+  apply eq.symm _,      -- application of symm *theorem*
+  apply eq.trans h1 h2, -- application of trans theorem
 end
 
 /-
 INTRODUCTION and ELIMINATION RULES
+-/
+
+/-
+For =
+  - introduction rule: eq.refl 
+  - elimination rule: eq.subst
+-/
+
+/-
+For ∀ x, P x
+  - introduction rule, assume arbitrary x, show P x
+  - elimination rule, next time!
 -/
 
