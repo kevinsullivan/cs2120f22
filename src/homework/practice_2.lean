57b636d52ec81fa255f80f7acbdfd11d6394e8f6
Kevin Sullivan <sullivan@virginia.edu>
2021-09-18 00:29:45 +0000

    Changes to homework in class

--- src/homework/practice_2.lean
+++ src/homework/practice_2.lean
@@ -6,12 +6,26 @@ in the sense that they identify all the axioms
 and/or theorems that you use.
 -/
 
-example : true := _
+example : true := true.intro
 
 example : false := _    -- trick question? why?
 
 example : ∀ (P : Prop), P ∨ P ↔ P := 
 begin
+  assume P, 
+  apply iff.intro _ _,
+  -- forward
+    assume porp,
+    apply or.elim porp,
+    -- left disjunct is true
+      assume p,
+      exact p,
+    -- right disjunct is true
+      assume p,
+      exact p,
+  -- backwards
+    assume p,
+    exact or.intro_left P p,
 end
 
 example : ∀ (P : Prop), P ∧ P ↔ P := 
