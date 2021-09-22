a47e1f120176ef762d9053100dd88ee37a69533b
Kevin Sullivan <sullivan@virginia.edu>
2021-09-22 18:09:29 +0000

    Update to most recent lecture 9

--- src/instructor/lectures/lecture_9.lean
+++ src/instructor/lectures/lecture_9.lean
@@ -11,64 +11,107 @@ such as way as to assert that P is not true.
 
 /-
 So what does it mean when we say that *it is
-true that P is not true*? 
+true that P is not true*? Or, equivalently, "it
+is true that P is false?" 
+  
+Clearly we don't mean that the proposition P 
+*is* (equal to) the proposition, *false*. No,
+in this sense of false, what we mean by P is
+false is that there are no proofs of P, so we
+have to *judge* it to be logically false. We
+distinguish between truth judgments and the
+proposition, true.
 -/
 
 /-
-First, if ¬P is true, there should be a proof
-of it. Second, what that proof should show is 
-that *there can be no proof of P*. 
+So what we mean by ¬P is that there is no 
+proof of P. The trick is now in how to show
+that there can be no proof of P. We prove
+it somewhat indirectly by proving P → false. 
+
+Suppose P → false is true. What this says is
+that if P is true then false is true. But the
+latter is absurd because we've defined false
+to the proposition with no proofs, so it can't
+be true.
+
+So if P → false is is true, then the consequence 
+of that is that there *can be no proofs of P*. 
 -/
 
 /-
-So the way we're going to say ¬P is to say
-if P were true then something that is completely
-impossible would happen. Because the impossible
-cannot happen, therefore there must be no proof
-of P.
+So let's launch a little exploration of proofs 
+involving false and proofs of false.
 -/
 
 /-
-What we're going take as "the impossible thing"
-is that there is a proof of false. Have defined
-false to be exactly a proposition with no proofs
-(otherwise it'd be true), so to have a proof of
-false is an impossibility.)
--/
+First, what's your intuition? Is the follow 
+proposition true or false? 
 
+  false → false 
+
+We'll let's see what happens when we try to 
+prove it. What the implication says is that
+*if* false is true, then false is true. To
+prove it, assume false is true and that we
+have a proof of it, f. All that remains is
+to produce a proof of false: just f itself.
+-/
 example : false → false :=
 begin
   assume f,
   exact f,
 end 
 
+/-
+Does false imply true? If the impossible happens, 
+is true still true? Turns out it is. Assume we've
+got a proof that false is true. Now all we have to
+do is to produce a proof of true. But it's an axiom
+that there is one: in Lean called true.intro. So
+it's true that false is true!
+-/
 example : false → true :=
 begin
   assume f,
   exact true.intro,
 end 
 
+/-
+Does true imply true? This one's easy as they get.
+-/
 example : true → true :=
 begin
   assume t,
   exact true.intro,
 end 
 
+
+/-
+Finally, does true imply false? Can you turn
+any proof of true somehow into a proof of false?
+If so, you have a proof of true → false. But the
+answer is no. Suppose you have a proof of true.
+Well, it's of no use at all in deriving a proof
+of false, and in fact you can never derive one
+from true premises, because our logic is sound(*).
+
+-/
 example : true → false :=
 begin
   assume t,
-  -- stuck
+  -- stuck: *there is no proof of false*
 end
 
-
-
 /-
-It's this analysis that leads to the definition
-of ¬P. For any proposition P, we *define* ¬P to
-be the proposition, P → false. What this means 
-is that if there is a proof of P → false, then
-you can conclude (by definition) ¬P. This is the
-introduction rule for ¬.
+Having built some intuition, let's get back to 
+the meaning of ¬P. For any proposition, P, we 
+*define* ¬P to be the proposition, P → false. 
+So ¬P is true exactly when P → false is true, 
+and that is true exactly when P is false, when
+there are no proofs of it. If you can produce a
+proof of P → false, then you can conclude ¬P. 
+This is the introduction rule for ¬.
 -/
 
 #check not    -- see definition in Lean library
@@ -81,96 +124,151 @@ that with that you can construct a proof of false.
 
 /-
 Example. Prove ¬ 0 = 1.
+
+Ok, we'll let's make another little diversion.
 -/
 
 example : false := 
-begin
+begin                     -- no way to prove this 
 end
 
 example : ¬ false := 
 begin
-  assume f,
+  assume f,               -- REMEMBER: ¬P *means* P → false, so *assume* P, show false.
   exact f,
 end
 
 example : ¬ (0 = 1) := 
 begin
   assume h,
-  
+  -- what do we do now?
 end
 
 
 /-
 To understand how to finish off this last
-proof, we need to talk more case analysis
-again. Remember that we've used it to reason
+proof, we need to talk about *case analysis*
+again. Remember that we used it to reason
 from a proof of a disjunction. Suppose we
 want to know that P ∨ Q → R. We start by 
 assuming that we have a proof, pq, of P ∨ Q, 
-and then we need to show that R follows as
-a logical consequence.
+then we need to show that R follows from
+the truth of P ∨ Q as a logical consequence.
 
-But there are exactly two possible forms that a
-proof of P ∨ Q can take (or.intro_left p) and
-(or.intro_right q), where p and q are proofs of
-P and of Q, respectively. What we therefore need
-to show is that no matter which of those two 
-forms of proof we have, of P ∨ Q, that the truth
-of R follows. So we do a case analysis on pq.
+But to know whether R follows from P ∨ Q, we
+need to know that it follows from each of them.
+Well, there are exactly two possible forms that 
+a proof, pq, of P ∨ Q can take (or.intro_left p)
+and (or.intro_right q), where p and q are proofs
+of P and of Q, respectively. What we therefore 
+need to show is that no matter which of the two
+cases, a proof of R follows. 
 
-In the first case, we assume P ∨ Q is true
-because P is (or.intro_left was used to create
-the proof). In this case we need to show that 
-P → R. In the second case, where P ∨ Q is true 
-because Q is (or.intro_right was used to create
-the proof of P ∨ Q), we need to show that Q → R.
-
-The general principle is that if you can show
-that a proposition, R, is true no matter which
-form of proof you have of some proposition, X, 
-then you have proven that X → R. This is the key
-idea behind *proof by case analysis*. Show that
-given any possible proof of P, that Q follows,
-and that's what gives you a proof of P → Q. A
-good start is to know just how many cases you
-have to consider! Given  proof of P ∨ Q, how many
-cases are there? Two.
+The rest of the proof is by case analysis on
+the proof, pq, of P ∨ Q. In the first case, we
+assume P ∨ Q is true because P is (or.intro_left 
+was used to create the proof). In this case we
+need to show that  P → R. In the second case,
+P ∨ Q is true because Q is (or.intro_right was
+used to create the proof of P ∨ Q. Now we need
+to show that having a proof of Q gets us to a 
+proof of R, i.e., that Q → R.
 -/
 
+
+-- template: proof of disjunction by case analysis
 example : ∀ (P Q R : Prop), P ∨ Q → R :=
 begin
   assume P Q R,
-  assume pq,
-  cases pq,       -- or elimination
+  assume pq,      -- assuming we have a proof, (pq : P ∨ Q)
+  cases pq,       -- proceed by case analysis
                   -- stuck here of course
 end
 
+
+/-
+We can now state the the general principle. If
+you have a proposition, X (such as P ∨ Q) that has
+several possible forms of proof, and you show that
+in each and every one of thoses cases, some other
+proposition R must be true then you've shown that
+X → R, because you've shown a way to convert any
+for of proof of X into a proof of R.
+-/
+
+/-
+So now we're lined up for a mind-bending count-down.
+
+As we've just said, there are two possible forms or
+cases of proofs for a disjunction, so when we do case
+analysis, we have to consider two cases. But now let's
+consider proposition, true → true. To prove it we'll
+start by assuming that we have a proof, t, of true. 
+A case analysis on this proof has only one case: the
+only way a proof of true can arise is by true.intro.
+-/
+
 example : true → true :=
 begin
-  assume t,
-  cases t,
+  assume t,   -- now we've assumed a proof of true
+  cases t,    -- there's just one case to consider
+  exact true.intro,   -- and it is easily proved
 end 
 
 
-example : ¬(0 = 1) :=
-begin
-  assume h,
-  cases h,
-end
-
+/-
+So now let's return to a proposition we already
+proved to be true, but now use case analysis in
+the proof.
+-/
 example : false → false :=
 begin
-  assume f,
-  cases f,
+  assume f,   -- assume we're given a proof, f, of false
+  cases f,    -- case analysis on f; zero cases; done!
 end
 
+/-
+Case analysis is the elimination principal
+for false. If one can derive a proof of false,
+what that really means is "this can't really
+happen," so if you assume it does, then, really
+anything goes. 
+-/
 example : false → false :=
 begin
-  assume f,
-  exact false.elim f,
+  assume f,             -- assumed proof of false (argument)
+  exact false.elim f,   -- false.elim in Lean
 end
 
+/-
+If you have a proof of false, then any proposition
+is true!
+-/
 theorem false_elim (P : Prop) (f : false) : P :=
 begin
-  cases f,
+  cases f,    -- try exact false.elim f
 end
+
+/-
+Finally, now, we're in a position to see how to 
+prove some not completely trivial propositions false.
+Let's prove that it's false that 0 = 1. In other words,
+let's prove, ¬0 = 1, I other words let's prove that
+0 = 1 → false. The way forward is to assume that we
+have a proof, say h, of 0 = 1, and then in this
+context we need a proof of false! 
+
+The only way to get there is to show false for
+every possible form of proof of 0 = 1. But the only
+form of proof of an equality is (eq.refl n). There
+are *zero* forms of proof for the proposition 0 = 1.
+A case analysis thus show that no matter which of
+the *zero* forms of proof you have, you get false.
+-/
+example : ¬(0 = 1) :=
+begin
+  assume h, -- assume a proof, h, of 0 = 1
+  cases h,  -- case analysis reveals this can't happen
+  -- we thus have a proof that 0 = 1 has no proofs
+end
+
