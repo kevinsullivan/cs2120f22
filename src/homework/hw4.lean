ade089aa9f6465794f2c3687b84ba5924e2537ff
Kevin Sullivan <sullivan@virginia.edu>
2021-09-24 19:44:52 +0000

    hw4

---
+++ src/homework/hw4.lean
@@ -0,0 +1,98 @@
+-- 1
+example : 0 ≠ 1 :=
+begin
+end
+
+
+-- 2
+example : 0 ≠ 0 → 2 = 3 :=
+begin
+end
+
+-- 3
+example : ∀ (P : Prop), P → ¬¬P :=
+begin
+end 
+
+-- We might need classical (vs constructive) reasoning 
+#check classical.em
+open classical
+#check em
+
+/-
+em : ∀ (p : Prop), p ∨ ¬p
+
+This is the famous and historically controversial
+"law" (now axiom) of the excluded middle. It's is
+a key to proving many intuitive theorems in logic
+and mathematics. But it also leads to giving up on
+having evidence *why* something is either true or
+not true, in that you no longer need a proof of 
+either P or of ¬P to have a proof of P ∨ ¬P.
+-/
+
+-- 4
+theorem neg_elim : ∀ (P : Prop), ¬¬P → P :=
+begin
+end
+
+-- 5
+theorem demorgan_1 : ∀ (P Q : Prop), ¬ (P ∧ Q) ↔ ¬ P ∨ ¬ Q :=
+begin
+end
+
+
+-- 6
+theorem demorgan_2 : ∀ (P Q : Prop), ¬ (P ∨ Q) → ¬P ∧ ¬Q :=
+begin
+end
+
+
+-- 7
+theorem disappearing_opposite : 
+  ∀ (P Q : Prop), P ∨ ¬P ∧ Q ↔ P ∨ Q := 
+begin
+end
+
+
+-- 8
+theorem distrib_and_or : 
+  ∀ (P Q R: Prop), (P ∨ Q) ∧ (P ∨ R) ↔
+                    P ∨ (Q ∧ R) :=
+begin
+end
+
+-- remember or is right associative
+-- you need this to know what the lefts and rights are
+-- 9
+theorem distrib_and_or_foil : 
+  ∀ (P Q R S : Prop),
+  (P ∨ Q) ∧ (R ∨ S) ↔
+  (P ∧ R) ∨ (P ∧ S) ∨ (Q ∧ R) ∨ (Q ∧ S) :=
+begin
+end
+
+
+/- 10
+Formally state and prove the proposition that
+not every natural number is equal to zero.
+-/
+lemma not_all_nats_are_zero : _ :=
+begin
+end 
+
+-- 11. equivalence of P→Q and (¬P∨Q)
+example : ∀ (P Q : Prop), (P → Q) ↔ (¬P ∨ Q) :=
+begin
+end
+
+-- 12
+example : ∀ (P Q : Prop), (P → Q) → (¬ Q → ¬ P) :=
+begin
+end
+
+-- 13
+example : ∀ (P Q : Prop), ( ¬P → ¬Q) → (Q → P) :=
+begin
+end
+
