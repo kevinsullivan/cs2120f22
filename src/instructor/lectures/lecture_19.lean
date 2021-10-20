29d4ce8f7e3aef948961325e146353b892b31c4d
Kevin Sullivan <sullivan.kevinj@gmail.com>
2021-10-20 22:42:55 +0000

    Lecture after class and separated out hw6

--- src/instructor/lectures/lecture_19.lean
+++ src/instructor/lectures/lecture_19.lean
@@ -10,46 +10,101 @@ the following conjectures.
 def evens : set ℕ := { n | n%2 = 0}
 
 example : ({ 0, 2 } : set ℕ) ⊆ evens :=
+/-
+Before looking at the proof script, spend
+a good bit of time trying to be sure that
+you see how the proof will work. We want
+to prove a subset relation, so we want to
+prove that for all values, v, if v is in
+the first set, then v is in the second. 
+The rest of the proof is by case analysis.
+v ∈ {0, 2} means v = 0 ∨ v = 2. These are
+the cases. In each case, we need to show
+that the value, v, is in the second set. 
+This means that the value satisfies the
+set membership predicate, so we will need
+to prove (evens 0) and (evens 2). Writing
+out the definition of evens, we'll need to
+prove 0 % 2 = 0 and 2 % 2 = 0, and these
+are true by the reflexivity of equality.
 
+Here we hope wou see how vitally important 
+it is to *learn* the basic definitions and
+to *look them up* again if you forget them 
+or get stuck. Always go to the definitions
+if you get stuck. That's how to learn or to
+*remember* what the symbols and other terms
+mean. Put all the smaller pieces together
+again, and often that will show you how to
+get unstuck. 
 
+This principle is particularly important
+right now because we've introduced a major
+new layer of abstractions, from logic to 
+set theory, and associated new notations
+by which to refer to these new concepts.
+You need to *memorize* the definitions of
+basic set notations to be able to write 
+propositions and produce proofs in set
+theory.
+-/
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+/-
+So here's a proof script in Lean. Use it
+to 
+-/
 
 begin
-  --show ∀ n, n = 0 ∨ n = 2 → n ∈ evens,
+  /-
+  Uncomment next line to see how to use show
+  to rewrite a goal to "definitionally equal" 
+  form. The rewriting here makes it easier to
+  see exactly when the first two moves are to
+  assume that you're given argument values, n
+  and h. This rewriting is not needed though 
+  for the rest of the proof to work as is.
+  -/
+  show ∀ n, n = 0 ∨ n = 2 → n ∈ evens,
   assume n,
   assume h,
   cases h,
+
   -- case: n = 0
   rw h,
- 
-  unfold evens,
-  show {n : ℕ | n % 2 = 0} 0,
-  show 0 % 2 = 0,
-  
+  /-
+  Uncomment the following lines if you want
+  to see in more detail why it makes sense 
+  that rfl is what's needed to finish the
+  proof. (But before you do, try to work it
+  out in your own head!)
+  -/
+  -- unfold evens,
+  -- show {n : ℕ | n % 2 = 0} 0,
+  -- show 0 % 2 = 0,
   exact rfl,
+
   -- case: n = 2
   cases h,
+  /-
+  At this point you have h, a proof of 2 = 2,
+  (albeit written in a strange way by Lean),
+  and you need to prove that 2 (writen weirdly)
+  is in the evens set. In this case, the h is
+  not needed, as the proof that "2 is even" is
+  trivial [ok, it's by reflexivity of equality]
+  -/
+
+  --uncomment for unnecessary gory details!
   /-
   show 2 ∈ evens,
   show evens 2,
   unfold evens,
   show 2 % 2 = 0,
+  show 0 = 0,
   -/
-  exact rfl,
+  exact rfl,    -- Ta Da!
 end
+
 
 /-
 We now look at the concept of *equality* 
@@ -78,89 +133,92 @@ namepace and then just call it ext.
 
 /-
 Remember that you can think about an
-implication, P → Q, in two ways: first,
-if P then Q; second, to prove Q it will
-suffice to prove P. So to prove L = X, 
-it suffices to prove ∀ x, x ∈ L ↔ x ∈ X,
-because one can then apply ext to that
-proof to derive a proof of L = X. In 
-other words, ext lets you "reduce" the
-need for a proof of L = X to the need
-for a proof of ∀ x, x ∈ L ↔ x ∈ X. And
-that is what we see next. 
+implication, P → Q, in two ways: first
+(reading left to right), if P then Q; 
+second (reading right to left), to show
+Q it will suffice to show P. Reading #2
+is what we want here. The salient point
+is that if you know P → Q and you need
+to show Q, then it will suffice to show
+P. P → Q thus allows you to reduce the
+problem of showing Q to the problem of
+showing P. This is the principle that 
+we use in this next concrete example.
 
-The concept of set equality, and the 
-need to prove certain sets to be equal,
-is extremely common, so it's important
-to master these concepts here.
+To prove L = X, it will suffice to prove 
+∀ x, x ∈ L ↔ x ∈ X. If one has a proof
+of ∀ x, x ∈ L ↔ x ∈ X, then one easily
+obtains a proof of L = X by applying the
+axiom of extensionality. Therefore (now
+go right to left), if we need to prove
+L = X, then it will suffice to have a
+proof of ∀ x, x ∈ L ↔ x ∈ X, as one 
+then just applies ext to get a proof of
+X = L. 
+
+Now look again at the definition of ext.
+
+  set.ext : 
+    ∀ {α : Type u_1} {a b : set α}, 
+    (∀ (x : α), x ∈ a ↔ x ∈ b) → a = b
+
+What is says is that we can apply ext 
+such a proof to derive a proof of L = X.
+
+Here's the most important point: If we 
+apply ext to a hole where the proof of
+the bi-implication should be, we will 
+have our proof of L = X with only the
+bi-implication proof remaining to be
+produced. In this sense, applying the
+axiom of set extensionality *without*
+giving a proof of the bi-implication,
+*reduces* the problem of proving L = X
+to the problem of proving ∀ x, x ∈ L ↔ 
+x ∈ X. And that is what we see next. 
 -/
 
 example : ∀ {α : Type} (L X : set α), L ⊆ X → ((L ∩ X) = L) := 
 begin
   intros α L X h,
-  apply set.ext _,  -- turn = into ↔ using extensionality axiom
-  -- that's the whole proof as long as we can fill in the _
-  -- but now that's just ordinary logical reasoning
+  apply set.ext _,  -- reduce = to ↔ by set extensionality
+  /- 
+  That's the whole proof as long as we can fill in the _
+  That's what the rest of this proof script then does. 
+  Notice again how "applying an implication theorem" 
+  can be used to reduce a current proof goal to goals 
+  the satisfaction of which "will suffice" to enable
+  construction of the proof that's needed.
+  -/
   assume x,
   split,
   -- forward
   assume h,
-  cases h,
-  exact h_left,
+  /- 
+  Remember, h is a proof of a conjunction
+  so "cases h" really does and elimination 
+  giving us the left and right subproofs as
+  the arguments that must have been given as
+  arguments to the and.intro that must have
+  been used to construct such a proof in 
+  the first place.
+  -/
+  cases h with l r,
+  exact l,
+  -- quiz: would "exact h.left" have worked?
+  -- predict the answer before checking
+
   -- backward
   assume k,
   exact and.intro k (h k),
+  /-
+  So this last "proof move" will take a little
+  time to think about. Look at the goal and think
+  for yourself what you really need to prove here.
+  Go back to the definitions! x ∈ L ∩ X really 
+  just means L x ∧ X x. Does this help you to see
+  why and.intro is required here, and what each 
+  of the terms in the preceding express means?
+  -/
 end 
-
-/-
-Give an English language proof of the preceding theorem.
--/
-
-
-/-
-Exercise: Prove that for any set, L, L ∩ L = L.
--/
-
-
-/-
-Exercise: Give a formal statement and proof, then an 
-English language proof, that the union operator on 
-sets is commutative.
--/
-
-
-/-
-Exercise: Prove that ⊆ is reflexive and transitive.
-Give a formal statement, a formal proof, and an English
-language (informal) proof.
--/
-
-
-/-
-Exercise: Prove that ∪ and ∩ are associative.
-Give a formal statement, a formal proof, and an 
-English language (informal) proof.
--/
-
-
-/-
-Assignment: read (at least skim) the Sections 1 and 2 of 
-the Wikipedia page on set identities: 
-https://en.wikipedia.org/wiki/List_of_set_identities_and_relations 
-There you will find definitions of left and right
-distributivity, which you will need for the following
-exercises.
--/
-
-/-
-Exercise: Prove (formally and informally) that 
-∩ is left-distributive over ∩.
--/
-
-
-/-
-Exercise: Prove (formally and informally) that 
-∪ is left-distributive over ∩.
--/
-
 
