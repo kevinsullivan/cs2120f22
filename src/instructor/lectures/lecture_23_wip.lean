0eba9a4907dbd77e01f732ab034efea4f703d05a
Kevin Sullivan <sullivan@virginia.edu>
2021-11-01 18:47:39 +0000

    .

---
+++ src/instructor/lectures/lecture_23_wip.lean
@@ -0,0 +1,99 @@
+import .lecture_21 
+
+/-
+ADDITIONAL PROPERTIES OF RELATIONS
+-/
+
+namespace relations
+
+section relation
+
+/-
+Relation, r, as two-place predicate on (pairs of)
+objects of a type, β, with infix notation, x ≺ y,
+for (r x y). 
+-/
+variables {α β : Type}  (r : β → β → Prop)
+local infix `≺`:50 := r  
+
+/-
+Let S be a type, id_S = {(a, a) | a ∈ S} and ir_S = (S × S) − id_S.
+Let α be a binary relation defined on S (i.e., α ⊆ S × S). We define
+i(α) = α ∩ ir_S and r(α) = α ∪ id_S. The relation α is said to be
+
+-- done
+– irreflexive if α ⊆ ir_S (equivalently, α ∩ id_S = ∅ or i(α) = α);
+– reflexive if id_S ⊆ α (or r(α) = α);
+– strictly (or sharply) antisymmetric if (a, b) ∈ α implies (b, a) ∉ α;
+– antisymmetric if a = b whenever (a, b) ∈ α and (b, a) ∈ α;
+– symmetric if (a, b) ∈ α implies (b, a) ∈ α;
+– transitive if (a, c) ∈ α whenever (a, b) ∈ α and (b, c) ∈ α;
+-/
+
+
+
+def total := ∀ x y, x ≺ y ∨ y ≺ x
+
+--
+def irreflexive := ∀ x, ¬ x ≺ x
+
+--
+def anti_symmetric := ∀ ⦃x y⦄, x ≺ y → y ≺ x → x = y
+
+-- "strictly anti-symmetric" above?
+def asymmetric := ∀ ⦃x y⦄, x ≺ y → ¬ y ≺ x  
+
+--
+def empty_relation := λ a₁ a₂ : α, false
+
+def subrelation (q r : β → β → Prop) := ∀ ⦃x y⦄, q x y → r x y
+
+inductive tc {α : Type} (r : α → α → Prop) : α → α → Prop
+| base  : ∀ a b, r a b → tc a b
+| trans : ∀ a b c, tc a b → tc b c → tc a c
+
+
+/-
+-/
+
+def identity_relation {α : Type} : α → α → Prop := λ a₁ a₂ : α, a₁ = a₂
+
+/- 
+Reflecting
+https://dml.cz/bitstream/handle/10338.dmlcz/142762/ActaCarolinae_048-2007-1_5.pdf,
+provide formal definition of each of the following properties of a binary relation, 
+r, on objects of a type, β. We will call r 
+
+– a quasiordering if r is reflexive and transitive;
+– a strict (or sharp) ordering if r is irreflexive and transitive;
+– a near-ordering if r is anti_symmetic and transitive;
+– a (reflexive) ordering if r is reflexive, antisymmetric and transitive;
+– a tolerance if r is reflexive and symmetric;
+– * an equivalence if r is reflexive, symmetric and transitive
+-/
+
+
+def quasiordering := reflexive r ∧ transitive r
+def strict_ordering := anti_symmetric r ∧ transitive r
+def near_ordering := irreflexive r ∧ transitive r
+def ordering :=      reflexive r ∧ anti_symmetric r ∧ transitive r -- new
+def partial_order := reflexive r ∧ anti_symmetric r ∧ transitive r
+def tolerance := reflexive r ∧ symmetric r
+
+
+end relation
+end relations
+
+/- 
+Pullback from binary relation along function and properties it preserves,
+straight from Lean 3 Community mathlib. 
+
+def inv_image (f : α → β) : α → α → Prop :=
+λ a₁ a₂, f a₁ ≺ f a₂
+
+lemma inv_image.trans (f : α → β) (h : transitive r) : transitive (inv_image r f) :=
+λ (a₁ a₂ a₃ : α) (h₁ : inv_image r f a₁ a₂) (h₂ : inv_image r f a₂ a₃), h h₁ h₂
+
+lemma inv_image.irreflexive (f : α → β) (h : irreflexive r) : irreflexive (inv_image r f) :=
+λ (a : α) (h₁ : inv_image r f a a), h (f a) h₁
+-/
