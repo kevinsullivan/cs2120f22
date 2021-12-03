05474a69240a798a25ecd678cf9f2295b6d0f206
Kevin Sullivan <sullivan@virginia.edu>
2021-12-03 20:08:42 +0000

    .

---
+++ src/homework/homework_final.lean
@@ -0,0 +1,18 @@
+/-
+Read, understand (collaborating if necessary) the material
+in chapter 17 of Jeremy Avigad's *Logic and Proof.* It's here:
+https://leanprover.github.io/logic_and_proof/the_natural_numbers_and_induction.html
+-/
+
+/-
+The following problems are listed in the Problems section of 
+the chapter. 
+
+#1. Solve probem #1, first sentence only.
+#2. Give a precise but informal proof for #2.
+#3. Give a precise but informal proof for #3.
+-/
+
+/-
+To test out of the final exam ...
+-/
