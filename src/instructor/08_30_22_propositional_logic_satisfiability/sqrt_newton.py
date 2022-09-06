fb20a6343c4f9abe981c8c7f23d75786981f0636
Kevin Sullivan <sullivan@virginia.edu>
2022-09-06 18:18:44 +0000

    Update square root example using Newton's method

--- src/instructor/08_30_22_propositional_logic_satisfiability/sqrt_newton.py
+++ src/instructor/08_30_22_propositional_logic_satisfiability/sqrt_newton.py
@@ -1,35 +1,19 @@
-# Python3 implementation of the approach
-
-# Function to return the square root of
-# a number using Newtons method
-def squareRoot(n, l):
-
-    # Assuming the sqrt of n as n only
+# A function to c the square root of
+# a number, n, to within a tolerance
+# of e, using Newtons method
+def squareRoot(n, e):
     x = n
-
-    # To count the number of iterations
-    count = 0
-
-    while (1):
-        count += 1
-
-        # Calculate more closed x
+    while (True):
         root = 0.5 * (x + (n / x))
-
-        # Check for closeness
-        if (abs(root - x) < l):
+        if (abs(root - x) < e):
             break
-
-        # Update root
         x = root
-
     return root
 
 
 # Driver code
 if __name__ == "__main__":
-
-    n = 327
-    l = 0.00001
-
-    print(squareRoot(n, l))
+    n = 900
+    sqrt_900 = squareRoot(n, 0.00001)
+    print("The approximate square root of ", n, " is ", sqrt_900)
+    print("The error is approximately ", abs(sqrt_900**2 - 900))
