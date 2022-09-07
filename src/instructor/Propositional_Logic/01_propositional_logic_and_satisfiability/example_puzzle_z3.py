9bb67e3a2e09d56e129b7a64c2e5e749d07eef5a
Kevin Sullivan <sullivan.kevinj@gmail.com>
2022-09-07 03:40:06 +0000

    .

--- src/instructor/Propositional_Logic/01_propositional_logic_and_satisfiability/example_puzzle_z3.py
+++ src/instructor/Propositional_Logic/01_propositional_logic_and_satisfiability/example_puzzle_z3.py
@@ -11,33 +11,27 @@ s = Solver()
 
 # 1. At least one suspect is guilty
 some_guilty = Or(C,D,H,M,E)
-# s.add(Or(C,D,H,M,E))
 
 # 2. If Chase is guilty and 
 #       Heath is innocent
 #    then Decker is guilty
 C_H_D = (Implies(And(C, Not(H)), D))
-# s.add(Implies(And(C, Not(H)), D))
 
 # 3. If Chase is innocent
 #    then Mullaney is innocent
 C_M = Implies(Not(C), Not(M))
-# s.add(Implies(Not(C), Not(M)))
 
 # 4. If Heath is guilty
 #    then Mullaney is guilty
 H_M = Implies(H,M)
-# s.add(Implies(H,M))
 
 # 5. Chase and Heath are not both guilty
 C_H = Not(And(C, H))
-# s.add(Not(And(C, H)))
 
 
 # 6. Unless Heath is guilty
 #    then Decker is innocent
 H_D = Implies(Not(H), Not(D))
-# s.add(Implies(Not(H), Not(D)))
 
 # a complete specification of what counts as a solution  
 # + here, of propositions, means "and"
@@ -45,12 +39,18 @@ puzzle = And(some_guilty, C_H_D, C_M, H_M, C_H, H_D)
 
 s.add(puzzle)
 
-# Check for (and if so, store) a model, returning sat or unsat
+# We could also have done thr following: 
+# s.add(Or(C,D,H,M,E))
+# s.add(Implies(And(C, Not(H)), D))
+# s.add(Implies(Not(C), Not(M)))
+# s.add(Implies(H,M))
+# s.add(Not(And(C, H)))
+# s.add(Implies(Not(H), Not(D)))
+
+# Check for sat, if so store a model and set isSat to sat, else unsat
 isSat = s.check()
 
-# if the "model checking" found a solution
-# then say so and give it, or say no such thing.
 if (isSat == sat) :
-    print("There's a solution! ", s.model())
+    print("Found a solution:\n", s.model())
 else :
-    print("There is no solution!")
+    print("There's no solution.")
