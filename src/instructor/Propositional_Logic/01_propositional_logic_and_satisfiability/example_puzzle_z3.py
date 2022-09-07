bf7e8a7f21cda92f423deefd99d4f32047d8be98
Kevin Sullivan <sullivan.kevinj@gmail.com>
2022-09-07 02:23:19 +0000

    Update original solution

--- src/instructor/Propositional_Logic/01_propositional_logic_and_satisfiability/example_puzzle_z3.py
+++ src/instructor/Propositional_Logic/01_propositional_logic_and_satisfiability/example_puzzle_z3.py
@@ -1,76 +1,44 @@
+# Ack: https://medium.com/galileo-onwards/logic-puzzle-2-74e0166a4176
+# From Hy Conrad’s Giant Book of Whodunit Puzzles (1999, pg 115–6). 
+
 from z3 import *
 
 # In the code Guilty is True and
 # Innocent is Not Guilty.(
-Chase, Decker, Ellis, Heath, Mullaney = z3.Bools(
-    [
-        "Chase",
-        "Decker",
-        "Ellis",
-        "Heath",
-        "Mullaney",
-    ])
+C, D, E, H, M = Bools(["Chase", "Decker", "Ellis", "Heath","Mullaney",])
 
 s = Solver()
 
-# 1. at least one is guilty
-s.add(
-    (Chase == True)
-    or (Decker == True)
-    or (Heath == True)
-    or (Mullaney == True)
-    or (Ellis == True)
-)
+# 1. At least one suspect is guilty
+s.add(Or(C,D,H,M,E))
 
-# 2. if Chase is guilty
-#        and Heath is innocent
+# 2. If Chase is guilty and 
+#       Heath is innocent
 #    then Decker is guilty
-s.add(
-    z3.If(
-        Chase == True    # condition
-        and Heath != True,
-        Decker == True,  # then
-        True,            # else
-    )
-)
+s.add(Implies(And(C, Not(H)), D))
 
-# 3. if Chase is innocent
+# 3. If Chase is innocent
 #    then Mullaney is innocent
-s.add(
-    z3.Implies(
-        Chase != True,
-        Mullaney != True
-    )
-)
+s.add(Implies(Not(C), Not(M)))
 
-# 4. if Heath is guilty
+# 4. If Heath is guilty
 #    then Mullaney is guilty
-s.add(
-    z3.Implies(
-        Heath == True,
-        Mullaney == True
-    )
-)
+s.add(Implies(H,M))
 
 # 5. Chase and Heath are not both guilty
-s.add(
-    z3.Not(
-        Chase == True
-        and Heath == True
-    )
-)
+s.add(Not(And(C, H)))
+
 
 # 6. Unless Heath is guilty
 #    then Decker is innocent
-s.add(
-    z3.If(
-        Heath == True,
-        True,
-        Decker != True,
-    )
-)
+s.add(Implies(Not(H), Not(D)))
 
+
+# Check for a model and return sat or unsat
 satis = s.check()
+
+# if the "model checking" found a solution
+# then say so and give it, or say no such thing.
 if (satis == sat) :
     print("There's a solution! ", s.model())
 else :
