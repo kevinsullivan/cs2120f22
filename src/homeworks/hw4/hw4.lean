2da21530d29dea9e3e4076ffa72a95d47dc8d085
Kevin Sullivan <sullivan.kevinj@gmail.com>
2022-10-07 14:57:26 +0000

    Homework #4

---
+++ src/homeworks/hw4/hw4.lean
@@ -0,0 +1,146 @@
+/-
+CS 2120 F22 Homework #4. Due Oct 13.
+-/
+
+/- #1A [10 points]
+
+Write a formal proposition stating that 
+logical and (∧) is associative. That is, 
+for arbitrary propositions, P, Q, and R,
+P ∧ (Q ∧ R) is true *iff* (P ∧ Q) ∧ R is, 
+too. Replace the placeholder (_) with your
+answer.
+-/
+
+def and_associative : Prop := 
+  _
+
+
+/- #1B [10 points]
+
+Give an English language proof. Identify
+the inference rules of predicate logic
+that you use in your reasoning.
+-/
+
+/-
+Answer: 
+-/
+
+/- #1C [5 points]
+
+Give a formal proof of the proposition.
+Hint: unfold and_associative to start.
+-/
+
+theorem and_assoc_true : and_associative :=
+begin
+end
+
+
+
+/- #2A [10 points]
+
+Write the proposition that ∨ is associative.,
+analogous to the proposition about ∧ in #1.
+-/
+
+def or_associative : Prop := 
+  _
+
+
+/- #2B [10 points]
+
+Write an English language proof of it, citing
+the specific inference rules you use in your
+reasoning.
+-/
+
+
+/- #2C [5 points]
+
+Complete the following formal proof.
+-/
+
+theorem or_associative_true : or_associative :=
+begin
+end
+
+
+/- #3A [10 points]
+Write a formal statement of the proposition.
+-/
+
+def arrow_transitive : Prop :=
+  _
+
+
+/- #3B [10 points]
+
+Write an English language proof of the proposition
+that for any propositions, X, Y, and X, it's the
+case that (X → Y) → (Y → Z) → (X → Z). In other
+words, implication is "transitive." Hint: Recall
+that if you have a proof of, say, X → Y, and you 
+have a proof of X, you can derive a proof of Y by
+arrow elimination. Think of it as applying a proof
+of an implication to a proof of its premise to get
+yourself a proof of its conclusion.
+-/
+
+
+/- #3C [5 points]. 
+Write a formal proof of it.
+-/
+
+
+/- #4
+Suppose that if it's raining then the streets
+are wet. This problem requires you to prove that
+if the streets are not wet then it's not raining.
+-/
+
+/- #4A [10 points]
+
+Start by writing the proposition in predicate
+logic by completing the following answer.
+-/
+
+def contrapositive : Prop :=
+  ∀ (Raining Wet : Prop), 
+    (Raining → Wet) → (¬Raining → ¬Wet)
+
+
+/- #4B [10 points]. 
+-/
+
+theorem contrapositive_valid : contrapositive :=
+begin
+
+/- #4C [5 points]. 
+
+Give an English language proof of it.
+-/
+
+
+/- #5. Extra credit.
+
+Complete the following formal proof of the 
+proposition that if for any proposition P, 
+P ∨ ¬P is true, then for any propositions, 
+X and Y, if it's not the case that X or Y
+is true then it is the case that ¬X and ¬Y 
+is true. 
+-/
+
+theorem demorgan1 : 
+  (∀ (P : Prop), P ∨ ¬ P) → 
+    ∀ (X Y : Prop), 
+      ¬(X ∨ Y) → (¬X ∧ ¬Y) :=
+begin
+assume em X Y nxory,
+cases (em X) with x nx,
+let foo := or.intro_left Y x,
+_
+end
+
