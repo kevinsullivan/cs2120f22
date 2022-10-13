314502146d6147d23064b55efaf5e3eaf50c292b
Kevin Sullivan <sullivan.kevinj@gmail.com>
2022-10-13 16:27:54 +0000

    .

--- src/instructor/03_Lean_Prover/01_Basic_Keywords/01_check.lean
+++ src/instructor/03_Lean_Prover/01_Basic_Keywords/01_check.lean
@@ -64,7 +64,84 @@ and because it's a proposition, it's type is also Prop
 #check true
 
 /-
-Finally, the logical expression, true, is also a proposition,
+The logical expression, true, is also a proposition,
 so it's type is also Prop.
 -/
 
+#check true.intro 
+
+/-
+Finally we again see that propositions, themselves
+of type, Prop, are also types; just like string or
+bool are of type, Type, but are themselves types as
+well. 
+-/
+
+#check bool
+#check tt
+
+#check true
+#check true.intro
+
+/-
+Proofs (or "proof objects" or "proof terms") in 
+Lean are values of logical/propositional types.
+It's strange, but every proposition is its own
+type in the higher-order constructive logic of
+Lean. That is to say, in type theory, which you
+now understand pretty well!
+-/
+
+/-
+Extra! So what's the type of Prop, or of Type?
+In the following diagrams, --> means "has type."
+So, for example, Prop has type Type 0, which in
+Lean we can and usually do write just as "Type."
+
+Prop   --> Type 0 --> Type 1 --> Type 2, ad inf.
+Sort 0 --> Sort 1 --> Sort 2 --> Sort 3, ad inf.
+-/
+
+-- You can see it for yourself using #check
+
+-- Here with computational types
+#check 0
+#check nat
+#check Type
+#check Type 0
+#check Type 1
+-- ad infinitum
+
+-- Here with logical types
+#check true.intro -- a proof is a value
+#check true       -- a proposition is a type "in" Prop  
+#check Prop       -- the type of Prop is Type 
+#check Type 1     -- etc.
+-- ad infinitum
+
+/-
+Prop gets special treatment. In particular, and two
+proofs of any propositional type are considered to be
+equal, because they are considered to be equally good
+*as proofs*. This is certainly not the case for values
+of types that "live in" Type. Take bool, for example.
+It has two obviously unqual values, tt and ff in Lean.
+But suppose we have an arbitrary proposition, P, and
+two arbitrary proofs of it. Lean will treat them as
+being equal. 
+-/
+
+example (P : Prop) (pf1 pf2 : P) : pf1 = pf2 := rfl
+
+/-
+Cool! Note by the way that here we used Python/C-style
+"code" by declaring named arguments before the colon.
+We could have avoided that by binding names to formal
+parameters after the colon, with the same effect.
+-/
+
+example : ∀ (P : Prop) (pf1 pf2 : P), pf1 = pf2 := 
+begin
+assume P p1 p2,
+exact rfl,
+end 
