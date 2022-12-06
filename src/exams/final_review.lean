84f7edeae034fe2b16b3be05d0fde0b578cf5a7c
Kevin Sullivan <sullivan.kevinj@gmail.com>
2022-12-06 20:26:06 +0000

    .

---
+++ src/exams/final_review.lean
@@ -0,0 +1,39 @@
+import data.set
+
+/-
+- existence propositions and proofs
+  -- introduction rule
+  -- elimination rule
+  -- proofs involving existence
+
+- set theory
+  -- membership predicates
+  -- set operations and notations
+    - membership, ∈ 
+    - intersection, ∩ 
+    - union, ∪ 
+    - complement, ᶜ 
+    - difference, \
+    - subset relation, ⊆, ⊂ 
+    - powerset 𝒫 (the set of all subsets of a given set)
+  -- propositions and proofs
+
+  - relations
+  -- membership predicates (on pairs of values)
+  -- domain, domain of definition, co-domain, range
+  -- properties of relations
+    - single-valuedness (functionality)
+    - reflexivity
+    - symmetry
+    - transitivity
+    - equivalence
+    - asymmetry
+    - anti-symmetry
+
+  - induction
+    -- inductive data definitions
+    -- induction axioms (for enumerated types and ℕ)
+    -- definition of recursive functions
+    -- construction of proofs by induction
+
+-/
