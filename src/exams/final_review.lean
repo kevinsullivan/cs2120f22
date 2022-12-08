1958a30bb38da68c4fac14b0a403aca31cc07fca
Kevin Sullivan <sullivan.kevinj@gmail.com>
2022-12-08 15:12:53 +0000

    .

--- src/exams/final_review.lean
+++ src/exams/final_review.lean
@@ -35,5 +35,17 @@ import data.set
     -- induction axioms (for enumerated types and ℕ)
     -- definition of recursive functions
     -- construction of proofs by induction
-
 -/
+
+/-
+I asked ChatGPT whether 0 = 1 implies 2 = 3. Here's 
+the answer I got. So just how smart is ChatGPT?
+
+"No, 0 does not equal 1 and therefore the statement 
+"0 = 1 implies that 2 = 3" is not true. In mathematics, 
+the equal sign (=) is used to indicate that two 
+expressions are equal in value. So, if 0 did equal 1, 
+then it would be true that 2 equals 3 because any 
+number multiplied by 0 is equal to 0. But since 0 
+does not equal 1, the statement is not true." 
+  -/
